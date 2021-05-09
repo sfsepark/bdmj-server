67165c5b184df5684676ce379cdb42742228c85a
sfsepark <sfsepark@gmail.com>
2021-05-09 19:41:52 +0900

    sync user model with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AuthModule } from './auth';
+import configuration from './config/configuration';
+import { User } from './user';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        host: configService.get<string>('MYSQL.HOST'),
+        port: configService.get<number>('MYSQL.PORT'),
+        dialect: 'mysql',
+        username: configService.get<string>('MYSQL.USER'),
+        password: configService.get<string>('MYSQL.PASSWORD'),
+        database: configService.get<string>('MYSQL.DATABASE'),
+        autoLoadModels: true,
+        models: [User],
+      }),
+    }),
+    AuthModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
