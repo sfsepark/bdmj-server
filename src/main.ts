b80070cd2a2e6788b108942373083c9cdb4726db
sfsepark <sfsepark@gmail.com>
2021-06-23 21:13:42 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
-  console.log('SERVER START');
 }
 bootstrap();
