a336c63baf0f97fdcab80679e95c1e310dd33b99
sfsepark <sfsepark@gmail.com>
2021-11-17 00:23:11 +0900

    Feature/auth google (#15)
    * 구글 로그인 방식 추가
    
    * 구글 안드로이드 / 구글 iOS엔드포인트 추가

--- src/auth/google/google.guard.ts
+++ src/auth/google/google.guard.ts
@@ -1,5 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { AuthGuard, IAuthGuard } from '@nestjs/passport';
+import {
+  generateGoogleIdTokenStrategyName,
+  GoogleTokenStrategyType,
+} from './google.strategy';
 
-@Injectable()
-export class GoogleTokenAuthGuard extends AuthGuard('google-id-token') {}
+export interface GoogleTokenAuthGuardConstruct {
+  new (): IAuthGuard;
+}
+
+const generateGoogleTokenAuthGuard = (
+  strategyType: GoogleTokenStrategyType,
+): GoogleTokenAuthGuardConstruct => {
+  const strategyName = generateGoogleIdTokenStrategyName(strategyType);
+
+  @Injectable()
+  class GoogleTokenAuthGuard extends AuthGuard(strategyName) {}
+
+  return GoogleTokenAuthGuard;
+};
+
+export const GoogleDefaultTokenAuthGuard = generateGoogleTokenAuthGuard(
+  GoogleTokenStrategyType.default,
+);
+export const GoogleWebTokenAuthGuard = generateGoogleTokenAuthGuard(
+  GoogleTokenStrategyType.web,
+);
+export const GoogleIOSTokenAuthGuard = generateGoogleTokenAuthGuard(
+  GoogleTokenStrategyType.iOS,
+);
+export const GoogleAndroidTokenAuthGuard = generateGoogleTokenAuthGuard(
+  GoogleTokenStrategyType.android,
+);
